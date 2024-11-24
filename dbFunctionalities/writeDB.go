b880317343d7aed2e111963d3a71d4e672b1f5f1
Joe-BN <joe.ngera@gmail.com>
2024-11-24 18:06:03 +0300

    user update function now working

--- dbFunctionalities/writeDB.go
+++ dbFunctionalities/writeDB.go
@@ -11,3 +11,13 @@ func CreateAcc(user config.User) error{
 	db.Close()
 	return err
 }
+
+func UpdateAcc(id int, user config.User) error {
+    db, _:= ConnectDB()
+    _, err := db.Exec(`
+        UPDATE Acc
+        SET name =?, email =?, password =?
+        WHERE id =?;
+    `, user.Name, user.Email, user.Password, id)
+    return err
+}
--- main.go
+++ main.go
@@ -4,23 +4,27 @@ import (
 	"fmt"
 	_ "maria-test/config"
 	db "maria-test/dbFunctionalities"
+	"maria-test/config"
 )
 
 func main(){
 	// Create and connect to the
     fmt.Println("Running test ...")
 
-	/*
+	
 	user1 := config.User{
 		Name: "Josh",
 		Email: "omollo@gmail.com",
 		Password: "1234567890",
 	}
+	/*
 	db.CreateAcc(user)
 	
 	db.CreateTable()
 	*/
 	
 	//deleting a user
-	db.DeleteData(1)
+	//db.DeleteData(1)
+
+	db.UpdateAcc(2, user1)
 }
