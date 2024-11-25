6df67ee858ebcd30a4b3993a9a17928a4c0d07f7
Joe-BN <joe.ngera@gmail.com>
2024-11-25 20:28:17 +0300

    the update.go file now works

--- dbFunctionalities/updateDB.go
+++ dbFunctionalities/updateDB.go
@@ -1,1 +1,15 @@
 package datab
+
+import (
+	"maria-test/config"
+)
+
+func UpdateAcc(id int, user config.User) error {
+    db, _:= ConnectDB()
+    _, err := db.Exec(`
+        UPDATE Acc
+        SET name =?, email =?, password =?
+        WHERE id =?;
+    `, user.Name, user.Email, user.Password, id)
+    return err
+}
--- dbFunctionalities/writeDB.go
+++ dbFunctionalities/writeDB.go
@@ -12,12 +12,3 @@ func CreateAcc(user config.User) error{
 	return err
 }
 
-func UpdateAcc(id int, user config.User) error {
-    db, _:= ConnectDB()
-    _, err := db.Exec(`
-        UPDATE Acc
-        SET name =?, email =?, password =?
-        WHERE id =?;
-    `, user.Name, user.Email, user.Password, id)
-    return err
-}
