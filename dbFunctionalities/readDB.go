c51c5e0d7c325f32da34d12b5ad3de35204d0e09
Joe-BN <joe.ngera@gmail.com>
2024-11-29 16:53:53 +0300

    making the Search functionality work

--- dbFunctionalities/readDB.go
+++ dbFunctionalities/readDB.go
@@ -1,1 +1,42 @@
 package datab
+
+
+import (
+    "fmt"
+)
+
+func SearchAcc(iden string) error {
+	db,_ := ConnectDB()
+	// Prepare the SQL query
+    stmt, err := db.Prepare("SELECT * FROM Acc WHERE id =?")
+    if err!= nil {
+        fmt.Println(err)
+        return err
+    }
+    defer stmt.Close()
+
+    // Execute the query with the ID parameter
+    rows, err := stmt.Query(iden)
+    if err!= nil {
+        fmt.Println(err)
+        return err
+    }
+    defer rows.Close()
+
+    // Fetch the result
+    for rows.Next() {
+        var (
+			id int
+            name   string
+            email string
+            password    string
+        )
+        err := rows.Scan(&id, &name, &email, &password)
+        if err!= nil {
+            fmt.Println(err)
+            return err
+        }
+        fmt.Printf("ID: %d, Name: %s, Email: %s Password %s\n", id, name, email, password)
+    }
+	return err
+}
--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	_ "maria-test/config"
 	db "maria-test/dbFunctionalities"
-	"maria-test/config"
+	_ "maria-test/config"
 )
 
 func main(){
 	// Create and connect to the
     fmt.Println("Running test ...")
 
-	
+	/*
 	user1 := config.User{
 		Name: "Josh",
 		Email: "omollo@gmail.com",
 		Password: "1234567890",
 	}
-	/*
+	
 	db.CreateAcc(user)
 	
 	db.CreateTable()
@@ -26,5 +26,6 @@ func main(){
 	//deleting a user
 	//db.DeleteData(1)
 
-	db.UpdateAcc(2, user1)
+	//db.UpdateAcc(2, user1)
+	db.Search("3")
 }
