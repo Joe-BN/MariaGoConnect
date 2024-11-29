c670164f163e577c3b168dbb169beca61ea9591f
Joe-BN <joe.ngera@gmail.com>
2024-11-29 21:28:51 +0300

    recreating the connectDB.go file

--- dbFunctionalities/connectDB.go
+++ dbFunctionalities/connectDB.go
@@ -1,42 +1,37 @@
-//connecting and disconnecting from the database in question
-
 package datab
 
-
 import (
-	"fmt"
-	"log"
-	"strconv"
+    "fmt"
+    "log"
 
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+    _ "github.com/go-sql-driver/mysql"
 
 	con "maria-test/config"
 )
 
 var DB *sql.DB
 
+func ConnectDB() (*sql.DB, error) {
+    dsn := fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s",
+        con.Config("MYSQL_USER"),
+        con.Config("MYSQL_ROOT_PASSWORD"),
+        con.Config("DB_PORT"),
+        con.Config("MYSQL_DATABASE"),
+    )
 
-func ConnectDB()(*sql.DB, error){
-	var err error
-	p := con.Config("DB_PORT")
-	port, err := strconv.ParseUint(p, 10, 32) //(variable, base, size)
-	if err != nil {
-		log.Println("Int conversion error !!!")
-	}
-	dsn := fmt.Sprintf("%s:%s@tcp(localhost:%d)/%s", con.Config("MYSQL_USER"), con.Config("MYSQL_ROOT_PASSWORD"), port, con.Config("MYSQL_DATABASE"))
     db, err := sql.Open("mysql", dsn)
     if err!= nil {
-		fmt.Println(err)
-        return nil, err	
+        log.Printf("Error connecting to database: %v", err)
+        return nil, err
     }
-    // Ping db to verify the connection
+
     err = db.Ping()
     if err!= nil {
-        fmt.Println(err)
-        return nil, err	
+        log.Printf("Error pinging database: %v", err)
+        return nil, err
     }
-    
-	//fmt.Println("Connnected to all databases successfuly !!!")
+
+    //log.Println("Connected to database successfully")
     return db, nil
 }
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	_ "maria-test/config"
 	db "maria-test/dbFunctionalities"
-	_ "maria-test/config"
 )
 
 func main(){
