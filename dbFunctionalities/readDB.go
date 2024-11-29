2cba95a865b1e87b6fe60fa7762aa55e87080675
Joe-BN <joe.ngera@gmail.com>
2024-11-29 17:01:49 +0300

    recreating the original reading script to run faster and offer better error handling

--- dbFunctionalities/readDB.go
+++ dbFunctionalities/readDB.go
@@ -1,42 +1,51 @@
 package datab
 
-
 import (
     "fmt"
+    "log"
 )
 
+// SearchAcc retrieves account data by ID
 func SearchAcc(iden string) error {
-	db,_ := ConnectDB()
-	// Prepare the SQL query
+    db, err := ConnectDB()
+    if err!= nil {
+        return err
+    }
+    defer db.Close()
+
     stmt, err := db.Prepare("SELECT * FROM Acc WHERE id =?")
     if err!= nil {
-        fmt.Println(err)
+        log.Println(err)
         return err
     }
     defer stmt.Close()
 
-    // Execute the query with the ID parameter
     rows, err := stmt.Query(iden)
     if err!= nil {
-        fmt.Println(err)
+        log.Println(err)
         return err
     }
     defer rows.Close()
 
-    // Fetch the result
+    var (
+        id   int
+        name string
+        email string
+        password string
+    )
+    var errors []error
+
     for rows.Next() {
-        var (
-			id int
-            name   string
-            email string
-            password    string
-        )
-        err := rows.Scan(&id, &name, &email, &password)
+        err = rows.Scan(&id, &name, &email, &password)
         if err!= nil {
-            fmt.Println(err)
-            return err
+            errors = append(errors, err)
+            continue
         }
         fmt.Printf("ID: %d, Name: %s, Email: %s Password %s\n", id, name, email, password)
     }
-	return err
+
+    if len(errors) > 0 {
+        return errors[0]
+    }
+    return nil
 }
